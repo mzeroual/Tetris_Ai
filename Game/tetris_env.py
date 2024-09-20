3468493651e700ea81038e45ef293119469d12c6
mohammedmeziane <med632002@gmail.com>
2024-09-20 21:38:07 +0200

    new tetris env

--- Game/tetris_env.py
+++ Game/tetris_env.py
@@ -27,7 +27,7 @@ class Figure:
         [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]],
         [[1, 2, 5, 6]],
     ]
-    
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -41,64 +41,51 @@ class Figure:
     def rotate(self):
         self.rotation = (self.rotation + 1) % len(self.figures[self.type])
 
+
 class TetrisEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
         super(TetrisEnv, self).__init__()
-        self.width = 10
-        self.height = 20
-        self.block_size = 30
-        self.board = np.zeros((self.height, self.width), dtype=int)
+        self.board_width = 10
+        self.board_height = 20
+        self.board = np.zeros((self.board_height, self.board_width), dtype=int)
         self.current_piece = None
         self.current_piece_position = [0, 0]
         
         # Define action and observation space
         self.action_space = spaces.Discrete(4)  # 0: left, 1: right, 2: down, 3: rotate
-        self.observation_space = spaces.Box(low=0, high=1, shape=(self.height, self.width), dtype=np.int)
+        self.observation_space = spaces.Box(low=0, high=1, shape=(self.board_height, self.board_width), dtype=np.int)
 
-        # Pygame setup
-        pygame.init()
-        self.screen = pygame.display.set_mode((self.width * self.block_size, self.height * self.block_size))
-        pygame.display.set_caption('Tetris')
-        self.clock = pygame.time.Clock()
-        
+        self.reset()
+
     def reset(self):
-        self.board = np.zeros((self.height, self.width), dtype=int)
-        self.current_piece = self.new_piece()
-        self.current_piece_position = [0, self.width // 2]
-        return self.board
-
-    def break_lines(self):
-        lines = 0
-        for i in range(1, self.height):
-            zeros = 0
-            for j in range(self.width):
-                if self.field[i][j] == 0:
-                    zeros += 1
-            if zeros == 0:
-                lines += 1
-                for i1 in range(i, 1, -1):
-                    for j in range(self.width):
-                        self.field[i1][j] = self.field[i1 - 1][j]
-        self.score += lines ** 2
+        self.board = np.zeros((self.board_height, self.board_width), dtype=int)
+        self.current_piece = Figure(0, self.board_width // 2)
+        self.current_piece_position = [0, self.board_width // 2]
+        return self.get_state()
 
     def step(self, action):
         # Apply action
         if action == 0:  # Move left
-            old_x = self.current_piece_position[1]
-            self.current_piece_position[1] -= 1
+            self.current_piece.x -= 1
             if self.intersects():
-                self.current_piece_position[1]=old_x
+                self.current_piece.x += 1
         elif action == 1:  # Move right
-            old_x = self.current_piece_position[1]
-            self.current_piece_position[1] += 1
+            self.current_piece.x += 1
             if self.intersects():
-                self.current_piece_position[1]=old_x
+                self.current_piece.x -= 1
         elif action == 2:  # Move down
-            self.current_piece_position[0] += 1
+            self.current_piece.y += 1
+            if self.intersects():
+                self.current_piece.y -= 1
+                self.freeze()
         elif action == 3:  # Rotate
-            self.current_piece = self.rotate_piece(self.current_piece)
+            self.current_piece.rotate()
+            if self.intersects():
+                self.current_piece.rotate()
+                self.current_piece.rotate()
+                self.current_piece.rotate()
         
         # Check for game over
         done = self.is_game_over()
@@ -107,48 +94,87 @@ class TetrisEnv(gym.Env):
         reward = self.calculate_reward()
         
         # Get new state
-        state = self.board
+        state = self.get_state()
         
         return state, reward, done, {}
 
     def render(self, mode='human'):
-        if mode == 'human':
-            self.screen.fill((0, 0, 0))  # Fill the screen with black
-            # Draw the board
-            for y in range(self.height):
-                for x in range(self.width):
-                    rect = pygame.Rect(x * self.block_size, y * self.block_size, self.block_size, self.block_size)
-                    pygame.draw.rect(self.screen, colors[self.board[y][x]], rect, 0)
+        pygame.init()
+        screen = pygame.display.set_mode((self.board_width * 30, self.board_height * 30))
+        pygame.display.set_caption("Tetris")
+        screen.fill((0, 0, 0))
 
-            # Draw the current piece
-            for i in self.current_piece[0]:
-                x = i % 4
-                y = i // 4
-                x_pos = self.current_piece_position[1] + x
-                y_pos = self.current_piece_position[0] + y
-                if 0 <= y_pos < self.height and 0 <= x_pos < self.width:
-                    rect = pygame.Rect(x_pos * self.block_size, y_pos * self.block_size, self.block_size, self.block_size)
-                    pygame.draw.rect(self.screen, colors[1], rect, 0)
+        for i in range(self.board_height):
+            for j in range(self.board_width):
+                pygame.draw.rect(screen, colors[self.board[i][j]], (j * 30, i * 30, 30, 30))
 
-            pygame.display.flip()
-            self.clock.tick(10)  # Limit to 10 frames per second
+        if self.current_piece is not None:
+            for i in range(4):
+                for j in range(4):
+                    if i * 4 + j in self.current_piece.image():
+                        pygame.draw.rect(screen, colors[self.current_piece.color],
+                                         ((self.current_piece.x + j) * 30, (self.current_piece.y + i) * 30, 30, 30))
+
+        pygame.display.flip()
 
     def new_piece(self):
-        return random.choice(Figure.figures)
+        return Figure(0, self.board_width // 2)
 
-    def rotate_piece(self, piece):
-        return piece[1:] + piece[:1]
+    def intersects(self):
+        intersection = False
+        for i in range(4):
+            for j in range(4):
+                if i * 4 + j in self.current_piece.image():
+                    if i + self.current_piece.y > self.board_height - 1 or \
+                            j + self.current_piece.x > self.board_width - 1 or \
+                            j + self.current_piece.x < 0 or \
+                            self.board[i + self.current_piece.y][j + self.current_piece.x] > 0:
+                        intersection = True
+        return intersection
+
+    def freeze(self):
+        for i in range(4):
+            for j in range(4):
+                if i * 4 + j in self.current_piece.image():
+                    self.board[i + self.current_piece.y][j + self.current_piece.x] = self.current_piece.color
+        self.break_lines()
+        self.current_piece = self.new_piece()
+        if self.intersects():
+            self.state = "gameover"
+
+    def break_lines(self):
+        lines = 0
+        for i in range(1, self.board_height):
+            zeros = 0
+            for j in range(self.board_width):
+                if self.board[i][j] == 0:
+                    zeros += 1
+            if zeros == 0:
+                lines += 1
+                for i1 in range(i, 1, -1):
+                    for j in range(self.board_width):
+                        self.board[i1][j] = self.board[i1 - 1][j]
+        self.score += lines ** 2
 
     def is_game_over(self):
-        # Implement game over logic
+        for j in range(self.board_width):
+            if self.board[0][j] > 0:
+                return True
         return False
 
     def calculate_reward(self):
-        # Implement reward calculation logic
-        return 0
+        # Reward for clearing lines
+        reward = 0
+        for i in range(self.board_height):
+            if all(self.board[i]):
+                reward += 1
+        return reward
 
-# Register the environment
-gym.envs.registration.register(
-    id='Tetris-v0',
-    entry_point='__main__:TetrisEnv',
-)
+    def get_state(self):
+        state = np.copy(self.board)
+        if self.current_piece is not None:
+            for i in range(4):
+                for j in range(4):
+                    if i * 4 + j in self.current_piece.image():
+                        state[self.current_piece.y + i][self.current_piece.x + j] = self.current_piece.color
+        return state
